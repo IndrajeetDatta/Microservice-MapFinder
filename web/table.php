df4abb99ff73967903d0f037b13941c3d1b306a2
IndrajeetDatta <datta.indrajeet@gmail.com>
2019-03-29 15:08:27 +0700

    Changed Style

--- web/table.php
+++ web/table.php
@@ -2,20 +2,36 @@
 <html>
 <head>
 	<title> Table</title>
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 </head>
 <body>
-	<h1> Saved Places </h1>
-	<table align="center">
-		<tr>
-			<td> Name</td>
-			<td> Latitude </td>
-			<td> Longitude </td>
-		</tr>
-		<tr>
-			<td> <?php echo $_POST['nameVal'] ?></td>
+		<div class="jumbotron">
+  			<h1 class="display-4" style="text-align: center"> Welcome to Google Map Coordinates Finder </h1>
+		</div>
+
+	<h1 align="center"> Your Saved Places </h1>
+
+	<table class="table">
+  <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">Name</th>
+      <th scope="col">Latitude</th>
+      <th scope="col">Longitude</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      		<td> <?php echo $_POST['nameVal'] ?></td>
 			<td> <?php echo $_POST['latVal'] ?></td>
 			<td> <?php echo $_POST['lngVal'] ?></td>
-		</tr>
-	</table>
+    </tr>
+  </tbody>
+</table>
+
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
 </body>
 </html>
